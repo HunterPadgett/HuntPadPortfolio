f22fedb1b3fcb635a7c7c93dc8921b7a8853f5bb
Hunter Padgett <gizmokabanjo@gmail.com>
2022-08-20 00:53:56 -0400

    home page editing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,52 @@
 import "../App.css";
-import React from 'react';
+import React from "react";
+import Typical from "react-typical";
+import { motion, Variants } from "framer-motion";
 
 function Home() {
-
-return (
-  <div id="home" class="homeImg ">
-    <main class="homeTextContainer">
-      <h1 class="" id="homeName">
-        Hi, I'm Hunter Padgett
-      </h1>
-      <p class="text-center" id="homeBody">
-        I'm a developer
-      </p>
-    </main>
-  </div>
-);
+  return (
+    <div id="home" class="homeImg ">
+      <main class="homeTextContainer">
+        <motion.h1
+          class=""
+          id="homeName"
+          initial={{ x: -1000 }}
+          animate={{ x: 0 }}
+          transition={{
+            type: "spring",
+            bounce: 0.3,
+            duration: 2.5,
+          }}
+        >
+          Hi, I'm Hunter
+        </motion.h1>
+        <p class="text-center" id="homeBody">
+          I'm a&nbsp;
+          <Typical
+            wrapper="t"
+            loop={Infinity}
+            steps={[
+              "web developer 💻",
+              3000,
+              "learner 📖",
+              3000,
+              "gamer 🕹️",
+              3000,
+              "coffee addict ☕",
+              3000,
+              "lifter 🏋️‍♂️",
+              3000,
+            ]}
+          />
+        </p>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
 
-
-// return ( 
+// return (
 //      <div class="homeImg">
 //         <h1 id="homeName">Hunter Padgett <br />
 //         Web Developer </h1>
