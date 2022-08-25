867d150daeb7a69d60ec6580218b9b98097ed6a9
Hunter Padgett <gizmokabanjo@gmail.com>
2022-08-24 23:02:13 -0400

    still styling and some links

--- src/components/Work.js
+++ src/components/Work.js
@@ -15,12 +15,23 @@ function Work() {
       <section class="py-5">
         <div class="container">
           <article class="workContainer m-3 p-3">
-            <a class="workImgLink" href="#" target="_blank" rel="noreferrer">
+            <a
+              class="workImgLink"
+              href="https://lit-crag-19257.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <img class="workImg" src={cthulhu} alt="video game project" />
             </a>
             <div class="workWords">
-              <h1 class="workTitle ">
-                <a href="#">Cthulhu Invades!</a>
+              <h1 class="workTitle p-1 mt-1">
+                <a
+                  href="https://lit-crag-19257.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Cthulhu Invades!
+                </a>
               </h1>
 
               <div class="workDesc">
@@ -40,15 +51,20 @@ function Work() {
               <img class="workImg" src={womanImg} alt="womans rights march" />
             </a>
             <div class="workWords">
-              <h1 class="workTitle">
-                <a href="https://hunterpadgett.github.io/project-one/#gsc.tab=0">
+              <h1 class="workTitle p-1 mt-1">
+                <a
+                  href="https://hunterpadgett.github.io/project-one/#gsc.tab=0"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Reproductive Rights Center
                 </a>
               </h1>
 
               <div class="workDesc">
-                A hub where you can keep up to date with the most recent news and leglislature
-                concerning women's reproductive rights rights both nationally and at a state level.
+                A hub where you can keep up to date with the most recent news
+                and leglislature concerning women's reproductive rights rights
+                both nationally and at a state level.
               </div>
             </div>
           </article>
@@ -57,18 +73,16 @@ function Work() {
               <img class="workImg" src={redsand} alt="exmaple project" />
             </a>
             <div class="workWords">
-              <h1 class="workTitle">
-                <a href="#">Title</a>
+              <h1 class="workTitle p-1 mt-1">
+                <a href="#" target="_blank" rel="noreferrer">
+                  Project 3 Coming Soon
+                </a>
               </h1>
 
               <div class="workDesc">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Eligendi, fugiat asperiores inventore beatae accusamus odit
                 minima enim, commodi quia, doloribus eius! Ducimus nemo
-                accusantium maiores velit corrupti tempora reiciendis molestiae
-                repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt
-                neque nulla unde ipsum dolores nobis enim quidem excepturi,
-                illum quos!
               </div>
             </div>
           </article>
